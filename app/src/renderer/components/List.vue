97b9a1d88529c78f584b78c7ff1e6d63748460f4
kimromi <hiromi19860101@gmail.com>
2017-04-30 14:42:18 +0900

    Initialize top page

---
+++ app/src/renderer/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Welcome.</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+    },
+    name: 'list-page'
+  }
+</script>
+
+<style scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
